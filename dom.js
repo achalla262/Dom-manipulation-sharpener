f5e53f8ae91bc6446985716a8a2a720ad316601b
achalla262 <achalla262@gmail.com>
2023-02-26 12:43:26 +0530

    task 5

--- dom.js
+++ dom.js
@@ -52,13 +52,50 @@
   //items[i].style.backgroundColor='#f4f4f4';
 //}
 
-var items = document.getElementsByClassName('list-group-item');
+//var items = document.getElementsByClassName('list-group-item');
 // console.log(items);
 // console.log(items[2]);
 // items[2].textContent="hello 3";
 // items[2].style.fontWeight="bold";
-items[2].style.backgroundColor="green";
+//items[2].style.backgroundColor="green";
 
-for(var i=0;i<items.length;i++){
-  items[i].style.fontWeight='bold';
-}
+//for(var i=0;i<items.length;i++){
+  //items[i].style.fontWeight='bold';
+//}
+
+
+//GETELEMENTSBYTAGNAME//
+//var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent="hello 2";
+ //li[1].style.fontWeight="bold";
+ //li[1].style.backgroundColor="yellow";
+
+//for(var i=0;i<li.length;i++){
+ //li[i].style.backgroundColor='#f4f4f4';
+//}
+
+//GETELEMENTSBYCLASSNAME//
+var items=document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[4]);
+//items[4].textContent="hello 5";
+//items[4].style.fontWeight="bold";
+// for(var i=0;i<items.length;i++){
+//   items[i].style.backgroundColor='pink';
+// }
+
+
+
+
+//GETELEMENTSBYTAGNAME//
+//  var li = document.getElementsByTagName('li');
+//  console.log(li);
+//  console.log(li[4]);
+//  li[4].textContent="item 5";
+//  li[4].style.fontWeight="bold";
+ 
+//  for(var i=0;i<li.length;i++){
+  // li[i].style.backgroundColor='#f4f4f4';
+// }
