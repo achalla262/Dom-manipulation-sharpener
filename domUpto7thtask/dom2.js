753f9438102a75880b4e9ec26d791757fd068f38
achalla262 <achalla262@gmail.com>
2023-03-01 18:09:01 +0530

    task 9

--- domUpto7thtask/dom2.js
+++ domUpto7thtask/dom2.js
@@ -8,6 +8,9 @@ form.addEventListener('submit',addItem);
 //Delete event
 itemList.addEventListener('click',removeItem);
 
+//Filter Event
+filter.addEventListener('keyup',filterItems);
+
 //Filter event
 filter.addEventListener('keyup',filterItems)
 
@@ -17,6 +20,9 @@ function addItem(e){
 
   //Get input value
   var newItem = document.getElementById('item').value;
+  var newItem1 = document.getElementById('description').value;
+  console.log(newItem1)
+
 
   //create new li element
   var li=document.createElement('li');
@@ -26,18 +32,29 @@ function addItem(e){
 
   //Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(" "+newItem1));
+
 
   //create del button element
   var deleteBtn=document.createElement('button');
+  var editBtn=document.createElement('Edit');
 
   //Add classes to del button
   deleteBtn.className='btn btn-danger btn-sm float-right delete';
+  editBtn.className='btn btn-primary btn-sm float-right delete';
+  
+
 
   //Append text node
   deleteBtn.appendChild(document.createTextNode('x'));
+  editBtn.appendChild(document.createTextNode('Edit'));
+
+
 
   //Append button to li
+  
   li.appendChild(deleteBtn);
+  li.appendChild(editBtn);
 
   //Append li to list
   itemList.appendChild(li);
@@ -53,6 +70,25 @@ function removeItem(e){
   }
 }
 
+//Filter Items
+function filterItems(e){
+  //convert text to lowercase
+  var text=e.target.value.toLowerCase();
+  //Get lis
+  var items=itemList.getElementsByTagName('li');
+  //convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName=item.firstChild.textContent;
+    var description=item.childNodes[1].textContent
+    if(itemName.toLowerCase().indexOf(text)!=-1 ||description.toLowerCase().indexOf(text)!=-1){
+      item.style.display='block';
+    }else{
+      item.style.display='none';
+    }
+
+    
+  })
+}
 
 
 
