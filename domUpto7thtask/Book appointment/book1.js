483d9122f064ae0c039d15be9ec8ad6bca0d0b37
achalla262 <achalla262@gmail.com>
2023-03-04 19:22:14 +0530

    task 11

--- domUpto7thtask/Book appointment/book1.js
+++ domUpto7thtask/Book appointment/book1.js
@@ -6,11 +6,26 @@ form.addEventListener('submit',addItem);
 function addItem(e) {
   e.preventDefault();
 
-  // getting input 
-  var newName = document.getElementById('name').value
-  var newEmail = document.getElementById('email').value
+//   // getting input 
+//   var newName = document.getElementById('name').value
+//   var newEmail = document.getElementById('email').value
 
-  // inserting in local storage
-  localStorage.setItem("name",newName);
-  localStorage.setItem("email",newEmail);
+//   // inserting in local storage
+//   localStorage.setItem("name",newName);
+//   localStorage.setItem("email",newEmail);
+// }
+
+
+var newName = document.getElementById('name').value;
+var newEmail = document.getElementById('email').value;
+
+
+let myObj={
+  name:newName,
+  email: newEmail,
+}
+
+let myObj_serialized=JSON.stringify(myObj);
+localStorage.setItem("key",myObj_serialized);
+let myObj_Deserialized=JSON.parse(localStorage.getItem("myObj"))
 }
