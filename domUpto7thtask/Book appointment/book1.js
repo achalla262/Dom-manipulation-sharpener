71683ae5be3a9f52cb16943f7a66d27ce1babe69
achalla262 <achalla262@gmail.com>
2023-03-05 14:23:52 +0530

    task 13

--- domUpto7thtask/Book appointment/book1.js
+++ domUpto7thtask/Book appointment/book1.js
@@ -19,21 +19,6 @@ function addItem(e) {
 var newName = document.getElementById('name').value;
 var newEmail = document.getElementById('email').value;
 
-//create new li element
-
-var data = document.createElement('li');
-
-// Add class
-
-data.className = 'list-group-item'
-
-//add text node with input value
-
-data.appendChild(document.createTextNode(newName));
-data.appendChild(document.createTextNode('  ' + newEmail));
-
-//append child
-form.appendChild(data);
 let myObj={
   name:newName,
   email: newEmail,
@@ -42,4 +27,40 @@ let myObj={
 let myObj_serialized=JSON.stringify(myObj);
 localStorage.setItem(myObj.email,myObj_serialized);
 let myObj_Deserialized=JSON.parse(localStorage.getItem("myObj"))
+
+
+
+//create new li element
+
+var data = document.createElement('li');
+
+// Add class
+
+data.className = 'list-group-item'
+
+//Create a delete button 
+var deleteBtn = document.createElement('input');
+deleteBtn.type = 'button';
+deleteBtn.value = 'delete';
+
+deleteBtn.onclick = () => {
+  localStorage.removeItem(myObj.email);
+  form.removeChild(data);
+};
+
+//add classes to delete button 
+deleteBtn.className = 'btn-delete';
+
+//Append Text node to delete 
+deleteBtn.appendChild(document.createTextNode('X'));
+
+//add text node with input value
+
+data.appendChild(document.createTextNode(newName));
+data.appendChild(document.createTextNode('  ' + newEmail));
+
+//append child
+form.appendChild(data);
+data.appendChild(deleteBtn)
+
 }
