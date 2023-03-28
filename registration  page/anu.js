3b09ae092bdd968be3fc734967a45f05d22ec147
achalla262 <achalla262@gmail.com>
2023-03-28 14:31:16 +0530

    task 12

---
+++ registration  page/anu.js
@@ -0,0 +1,8 @@
+let x="anu";
+let z="feroz";
+let y="sandeep";
+let o="raki";
+let p="h";
+let n=23;
+
+console.log(`${x} is smart ${z} loves girls ${y} loves anime ${o} loves accounts he is ${p} he likes ${n}`)
